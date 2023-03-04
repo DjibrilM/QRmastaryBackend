bf1a3b0d88e40ea6871f04b9ead0fa9256857ad0
djibrilM <82331014+DjibrilM@users.noreply.github.com>
2023-03-05 01:14:42 +0300

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,7 @@
 import mongoose, { mongo } from "mongoose";
 
 
+
 export default class database {
  static    connect() {
         mongoose.connect("mongodb+srv://djibrilM:djibril2004@cluster0.qep8sf2.mongodb.net/?retryWrites=true&w=majority")
